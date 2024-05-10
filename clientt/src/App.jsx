54b19f8e6cfc477d176b906c62489ec2465c5236
sanju9922 <sanjaykumar090902@gmail.com>
2024-05-10 13:20:21 +0530

    Install React and tailwind css and create first template

---
+++ clientt/src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-3xl text-red-500'>
+      App
+    </h1>
+  )
+}
