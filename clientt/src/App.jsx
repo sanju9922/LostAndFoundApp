0e547a4a16965e4af5f21ea1f747bca27f4ad783
sanju9922 <sanjaykumar090902@gmail.com>
2024-05-10 16:27:21 +0530

    Created pages and routes

--- clientt/src/App.jsx
+++ clientt/src/App.jsx
@@ -1,10 +1,24 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import DashBoard from './pages/DashBoard'
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>
-      App
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/> }/>
+        <Route path="/about" element={<About/> }/>
+        <Route path="/signup" element={<SignUp/> }/>
+        <Route path="/signin" element={<SignIn/> }/>
+        <Route path="/dashboard" element={<DashBoard/> }/>
+        <Route path="/projects" element={<Projects/> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
